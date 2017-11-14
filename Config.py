6d6023ccf2696b421909a175ac8b5bd167d61785
huangqq <huang@qq.com>
2017-11-15 01:38:18 +0800

    i'i'i

--- Config.py
+++ Config.py
@@ -5,7 +5,7 @@ class Config(object):
 
 class ProductionConfig(Config):
     DB = '127.0.0.1'
-    PORT = 65521
+    PORT = 27017
     DBUSERNAME = 'scan'
     DBPASSWORD = 'scanlol66'
     DBNAME = 'xunfeng'
